76312448ee56e9f454b74ed6627e20d754dc6404
Greger Hälltorp <greger.halltorp@gmail.com>
2016-09-20 22:34:22 +0200

    added fatal()

--- logger/index.js
+++ logger/index.js
@@ -1,12 +1,25 @@
 /* eslint-disable no-console */
+
+const consoleLog = (message, error, callback) => {
+  console.log(message, error || '');
+  if (callback) {
+    callback();
+  }
+};
+
 export default {
   error(message, error, callback) {
     if (process.env.NODE_ENV !== 'production') {
-      console.log(message, error);
-      callback();
+      consoleLog(message, error, callback);
     } else {
-      console.log(message, error);
-      callback();
+      consoleLog(message, error, callback);
+    }
+  },
+  fatal(message, error, callback) {
+    if (process.env.NODE_ENV !== 'production') {
+      consoleLog(message, error, callback);
+    } else {
+      consoleLog(message, error, callback);
     }
   },
 };
